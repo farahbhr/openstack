8c8647486a106983fe70a0acc79e0cc1a3d944fb
Farah Bel Haj Rhouma <54190328+farahbhr@users.noreply.github.com>
2021-10-05 22:17:13 +0100

    Create provider.tf

---
+++ openstack/3tier/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+      openstack = { 
+            source = "terraform-provider-openstack/openstack"
+      }
+  }
+}
+
+
+
+provider "openstack" {
+  user_name   = "admin"
+  tenant_name = "admin"
+  password    = var.passwd
+  auth_url    = var.auth_url
+  region      = "RegionOne"
+  use_octavia   = true
+}
