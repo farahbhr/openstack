b39c0cd06554852f7668b4f9045e1ec5cff9dd52
Farah Bel Haj Rhouma <54190328+farahbhr@users.noreply.github.com>
2021-10-05 22:29:22 +0100

    Update routers.tf

--- openstack/2tier/routers.tf
+++ openstack/2tier/routers.tf
@@ -5,11 +5,6 @@ resource "openstack_networking_router_v2" "router_1" {
   external_network_id = "${openstack_networking_network_v2.external.id}"
 }
 
-resource "openstack_networking_router_interface_v2" "int_web" {
-  router_id  = "${openstack_networking_router_v2.router_1.id}"
-  subnet_id  = "${openstack_networking_subnet_v2.subnet_web.id}"
-}
-
 resource "openstack_networking_router_v2" "router_2" {
   name           = "router_2"
   admin_state_up = "true"
