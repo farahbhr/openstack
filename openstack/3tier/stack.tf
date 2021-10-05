28e9d82f6a2239bc984fd81b0b59a9ea8816b0a8
Farah Bel Haj Rhouma <54190328+farahbhr@users.noreply.github.com>
2021-10-05 22:16:55 +0100

    Update stack.tf

--- openstack/3tier/stack.tf
+++ openstack/3tier/stack.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_providers {
-      openstack = { 
-            source = "terraform-provider-openstack/openstack"
-      }
-  }
-}
-
-
-# Configurformie the OpenStack Provider
-provider "openstack" {
-  user_name   = "admin"
-  tenant_name = "admin"
-  password    = var.passwd
-  auth_url    = var.auth_url
-  region      = "RegionOne"
-  use_octavia   = true
-}
-
-
 
 resource "openstack_networking_network_v2" "external" {
   name           = "external"
